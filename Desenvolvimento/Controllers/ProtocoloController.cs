99a1c0e670c42e26b6d1dd3139ee9fa8e67ea0fb
guga56 <gustavogregorio086@gmail.com>
2023-08-01 15:43:00 -0300

    projeto incializado, com layout

---
+++ Desenvolvimento/Controllers/ProtocoloController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Desenvolvimento.Controllers
+{
+    public class ProtocoloController : Controller
+    {
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        public IActionResult Consultar()
+        {
+            return View();
+        }
+    }
+}
